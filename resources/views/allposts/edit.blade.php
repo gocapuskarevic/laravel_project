1793984fb50985c03c0f4859d52ad08093d868b1
goca_p <s_goca@yahoo.com>
2020-06-11 23:17:08 +0200

    Style changes.

--- resources/views/allposts/edit.blade.php
+++ resources/views/allposts/edit.blade.php
@@ -11,17 +11,16 @@
     </div><br />
 @endif
     <div class="row">
-    <form method="post" action="{{action('PostsController@update', $current->id)}}" >
+    <form method="post" action="{{action('PostsController@update', $current->id)}}" style="width:100%;">
     @csrf
        
         <div class="form-group">
-       
             <label for="title">Post Title:</label>
             <input type="text" class="form-control" name="post-title" value={{$current->title}} />
         </div>
         <div class="form-group">
             <label for="content">Post Content:</label>
-            <textarea cols="5" rows="5" class="form-control" name="post-content">{{$current->body}}</textarea>
+            <textarea rows="20" class="form-control" name="post-content">{{$current->body}}</textarea>
         </div>
         <button type="submit" class="btn btn-primary">Update</button>
         </form>
--- resources/views/allposts/post.blade.php
+++ resources/views/allposts/post.blade.php
@@ -4,17 +4,20 @@
   <h1>{{$current->title}}</h1>
   <p>{{$current->body}}</p>
 
-  @if( session('admin') && (session('admin') == 'success' ))
-
-    {!!Form::open(['action'=>['PostsController@destroy',$current->id],'method'=>'POST'])!!}
-      {{Form::hidden('_method', 'DELETE')}}
-      {{Form::submit('Delete post', ['class' => 'btn btn-danger'])}}
-    {!!Form::close()!!}
-
-    <a href="{{url('/posts/edit/'.$current->id)}}">Edit post</a>
-
-  @endif
-
   <a class="btn btn-primary" href="{{ URL::previous() }}">Vrati se nazad!</a>
+  <br>
+    <div  style="float:right;">
+      @if( session('admin') && (session('admin') == 'success' ))
+
+      {!!Form::open(['action'=>['PostsController@destroy',$current->id],'method'=>'POST'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete post', ['class' => 'btn btn-danger'])}}
+      {!!Form::close()!!}
+      <br>
+      <a href="{{url('/posts/edit/'.$current->id)}}" class="btn btn-secondary" style="float:right;">Edit post</a>
+
+      @endif
+    </div>
+  
 
 @endsection
